216fd47af376fc7e09fd5272a201754c252468da
GenaBitu <developer@dedic.eu>
2019-12-11 18:28:36 +0100

    Fixed task1

--- experiments/task1.jl
+++ experiments/task1.jl
@@ -3,7 +3,7 @@ using AbstractAlgebra;
 
 R, (x, y) = PolynomialRing(ZZ, ["x", "y"], ordering = :lex);
 
-f = x*y + 1;
+f = x*y + 2; # Originaly was x*y + 1
 g = x^2 + x - 2y;
 
 I = Ideal(R, [f, g]);
