a0b6028824708256e912c86fa043531e654195e5
GenaBitu <developer@dedic.eu>
2019-12-11 18:24:56 +0100

    Fixed variable name collision in buchberger

--- src/buchberger.jl
+++ src/buchberger.jl
@@ -2,19 +2,19 @@ using AbstractAlgebra;
 
 export buchberger;
 
-function buchberger(i::Ideal{T})::Ideal{T} where {T<:AbstractAlgebra.MPolyElem}
-	if i.is_groebner
-		return i;
+function buchberger(ideal::Ideal{T})::Ideal{T} where {T<:AbstractAlgebra.MPolyElem}
+	if ideal.is_groebner
+		return ideal;
 	end
-	generators = gens(i);
+	generators = gens(ideal);
 	for (i, g1) in enumerate(collect(generators))
 		for g2 in collect(generators)[i + 1:end]
 			reduced = reducePolyStar(SPolynomial(g1, g2), generators);
-			if reduced != zero(i)
+			if reduced != zero(ideal)
 				push!(generators, reduced);
-				return buchberger(Ideal(base_ring(i), generators, false));
+				return buchberger(Ideal(base_ring(ideal), generators, false));
 			end
 		end
 	end
-	return Ideal(base_ring(i), generators, true);
+	return Ideal(base_ring(ideal), generators, true);
 end
