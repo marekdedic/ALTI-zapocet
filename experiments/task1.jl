27ae5d2ca8f5bc7e856eabdb507c3ac411c950e0
GenaBitu <developer@dedic.eu>
2019-12-14 21:19:13 +0100

    Added complete task 1

--- experiments/task1.jl
+++ experiments/task1.jl
@@ -1,11 +1,13 @@
 using GroebnerBasis;
 using AbstractAlgebra;
 
-R, (x, y) = PolynomialRing(ZZ, ["x", "y"], ordering = :lex);
+R, (x, y) = PolynomialRing(QQ, ["x", "y"], ordering = :lex);
 
 f = x*y + 2; # Originaly was x*y + 1
 g = x^2 + x - 2y;
 
 I = Ideal(R, [f, g]);
 
-buchberger(I);
+I2 = buchberger(I)
+I3 = reduceBase(I2)
+I4 = normalizeBase(I3)
