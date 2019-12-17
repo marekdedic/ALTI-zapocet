e79a2559f1ccbbf20679987316d48d0add0da908
GenaBitu <developer@dedic.eu>
2019-12-17 09:47:39 +0100

    Added kovar2 experiment

---
+++ experiments/kovar2.jl
@@ -0,0 +1,15 @@
+using GroebnerBasis;
+using AbstractAlgebra;
+
+R, (x, y) = PolynomialRing(QQ, ["x", "y"], ordering = :deglex);
+
+x > y
+
+g1 = x^2 - 2y^2;
+g2 = x*y - 3;
+
+I = Ideal(R, [g1, g2]);
+
+I2 = buchberger(I)
+I3 = reduceBase(I2)
+I4 = normalizeBase(I3)
